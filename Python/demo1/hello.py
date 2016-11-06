8b3393537b39217af1acc0c02744e6493b092f56
hardCTE <84001791@qq.com>
2016-11-06 20:40:40 +0800

    杨辉三角

--- Python/demo1/hello.py
+++ Python/demo1/hello.py
@@ -57,5 +57,39 @@ print('~~~~~~~~~~')
 
 
 # 杨辉三角
+#          1
+#        1   1
+#      1   2   1
+#    1   3   3   1
+#  1   4   6   4   1
+#1   5   10  10  5   1
 
+def yhsj(max):
+    a,b,n=[],[1],1
+    while max>=n:
+        if n>1:
+            a,b=b,[]
+            for x in range(0,n):
+                if x==0 or x==a.__len__():
+                    b.append(1)
+                else:
+                    b.append(a[x-1]+a[x])
+        print(b)
+        n=n+1
 
+print('~~~~yhsj(0)~~~~~~')
+yhsj(0)
+print('~~~~yhsj(1)~~~~~~')
+yhsj(1)
+print('~~~~yhsj(2)~~~~~~')
+yhsj(2)
+print('~~~~yhsj(3)~~~~~~')
+yhsj(3)
+print('~~~~yhsj(5)~~~~~~')
+yhsj(5)
+print('~~~~yhsj(10)~~~~~~')
+yhsj(10)
+print('~~~~yhsj(20)~~~~~~')
+yhsj(20)
+
+print('~~~~~~~~~~')
